8849fc9350a2bb9f6a10ce16072c339ecf7d051b
William Holt <halbrd@gmail.com>
2019-12-22 20:25:40 +1100

    add dat boi to homepage

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'insert datboi here'
+    return render_template('pages/index.html')
 
 if __name__ == '__main__':
     # debug mode - not for production
